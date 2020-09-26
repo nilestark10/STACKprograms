535b3596422d9487ae23348e7999eb642b0e42e2
nilestark10 <srijan4790@gmail.com>
2020-09-26 19:06:26 +0530

    Create create_stack.c

---
+++ create_stack.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+# define Max 5
+
+typedef struct
+{
+    int data[Max];
+    int top;
+}STACK;
+
+int push()
+{
+    
+}
+
+int pop()
+{
+    
+}
+
+int main()
+{
+    STACK s1;
+    int m;
+    return 0;
+}
+
